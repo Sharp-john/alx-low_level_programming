e03af62838bca21dc4177b46b122cfd35d7ffee7
john <john.wanjiru@hotmail.com>
2023-03-19 21:34:56 +0300

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#inlclude < stdio.h >
+/**
+* main-prints the alphabet in lowercase, and then in uppercase
+* return:0
+**/
+int main(void)
+{
+	char alpha = 'a';
+	char Alpha = 'A';
+	int x;
+
+	for (x = 0; x < 26; x++)
+	{
+		putchar (a);
+		putchar (A);
+		Alpha = Alpha + 1;
+		alpha = alpha + 1;
+	}
+putchar ('\n');
+return (0);
+}
