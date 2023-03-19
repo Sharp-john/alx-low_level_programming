0018d871f88fc3bf7af1e9bc8864dcd1ef6df46c
john <john.wanjiru@hotmail.com>
2023-03-19 21:04:56 +0300

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
 * main - prints the alphabet in lowercase
-*Return 0
-*/ 
-int main ()
+* Return 0
+**/
+int main(void)
 {
 	char alpha = 'a';
 	int counter;
-	
-	for (counter = 0; counter < 27; counter ++)
+
+	for (counter = 0; counter < 27; counter++)
 	{
 		putchar(alpha);
-		putchar('\n);
-		alpha++;
+		putchar('\n');
+		alpha = alpha + 1;
 	}
-return 0;
+return (0);
 }
