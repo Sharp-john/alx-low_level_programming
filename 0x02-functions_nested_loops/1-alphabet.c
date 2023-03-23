d24d1f5c1356bd12f1d2ba52a77ea9bc91bce427
john <john.wanjiru@hotmail.com>
2023-03-23 05:28:05 +0300

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*main - printing the alpabest 10X
+*Return:0;
+**/
+int main(void)
+{
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar (x);
+		_putchar ("\n");
+	}
+}
+return (0);
+}
+
