e69b5d50627c046c134f700da80fca3a18a76b96
john <john.wanjiru@hotmail.com>
2023-03-28 20:47:02 +0300

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strlen-get length of string
+*@s:function param
+*Return: lenhth of char
+**/
+int _strlen(char *s)
+{
+	int l;
+
+	l = strlen(s);
+	return (l);
+}
