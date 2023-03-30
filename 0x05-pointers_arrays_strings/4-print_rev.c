2b8b3080e010680e74a617476547061735ed7447
john <john.wanjiru@hotmail.com>
2023-03-30 15:21:16 +0300

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+*print_rev - print in reverse oder
+*@s:char type
+**/
+void print_rev(char *s)
+{
+	int l = strlen(s);
+	int x;
+
+	for (x = l ; x >= 0 ; x--)
+	_putchar (s[x]);
+}
