459b1a4972c5f28c02e3f4d13d4a86a497dcb76a
john <john.wanjiru@hotmail.com>
2023-04-02 15:33:49 +0300

    functions

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*print_last_digit - prints last digit
+*@q:the parameter
+*Return:Last digit
+**/
+int print_last_digit(int q)
+{
+	int last;
+
+	last = q % 10;
+	return (last);
+}
