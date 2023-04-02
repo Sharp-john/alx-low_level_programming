68fde93feb0d886fc078baec0bd3402cdcbfd8f6
john <john.wanjiru@hotmail.com>
2023-04-02 14:51:05 +0300

    functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*_abs - absolute value of an int
+*@y:prameter value
+*Return:absolute value
+**/
+int _abs(int y)
+{
+	int x = abs(y);
+
+	return (x);
+}
